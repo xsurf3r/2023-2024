8b0df5b809e81549f1e39fa2283c5a762bb57a4c
d17 <vilanuvidusskola.d17@gmail.com>
2023-09-04 15:33:43 +0300

    AAAs

---
+++ 04.09.py
@@ -0,0 +1,17 @@
+#print(125 + 24) 
+#print(125 - 24)
+#print (125 * 24) 
+#print(125 / 24) 
+
+#inch= float(input('ievadi inch: '))
+
+#m = inch * 0.0254
+
+#print (m)
+
+
+#baze= float(input('ievadi baze: '))
+#augstums= float(input('ievadi augstums: '))
+#laukums = 1/2*baze*augstums
+
+#print(int(laukums))
